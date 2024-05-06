b1387b010df14b311dadf221faa60597fa5ad887
harrison c parker <hcp111@me.com>
2024-05-06 22:24:00 +0100

    DO NOT MERGE - holding page update - DO NOT MERGE

--- src/components/holding-page.js
+++ src/components/holding-page.js
@@ -5,13 +5,11 @@ export default function HoldingPage() {
     return (
         <section>
             <WrapperSection>
-                <div className="flex h-dvh items-center justify-center">
-                    <div>
-                        <Logo />
-                        <p className="text-4xl text-center mt-12 text-bold">
-                            Permiering 9th May 6pm BST
-                        </p>
-                    </div>
+                <div className="flex flex-col h-dvh items-center justify-center">
+                    <Logo />
+                    <p className="text-4xl text-center mt-12 text-bold">
+                        Premiering 9th May 6pm BST
+                    </p>
                 </div>
             </WrapperSection>
         </section>
