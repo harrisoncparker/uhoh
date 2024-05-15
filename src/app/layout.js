a02ff63ea391e3509bcc6a0d3aa146974356590e
harrison c parker <hcp111@me.com>
2024-05-15 09:50:08 +0100

    favicon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/wve7nip.css" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </head>
       <body className="bg-brand-light text-brand-dark text-xl sm:text-2xl font-body pt-1">
         <main>
